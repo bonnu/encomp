bc13e51a0a6318b4b23c8c915e877d373081922d
bonnu <boxphere@cpan.org>
2010-02-10 16:37:58 +0900

    元に戻した

--- lib/Encomp/Class/Encompasser.pm
+++ lib/Encomp/Class/Encompasser.pm
@@ -3,7 +3,6 @@ package Encomp::Class::Encompasser;
 use Encomp::Class;
 use Encomp::Complex;
 use Encomp::Meta::ProcessingNode;
-use Scalar::Util ();
 
 Encomp::Class->setup_metadata(node => sub { Encomp::Meta::ProcessingNode->new });
 
@@ -13,10 +12,10 @@ sub build {
 }
 
 sub operate {
-    my ($class, @args) = @_;
-    my $obj   = Scalar::Util::blessed($class) ? $class : build($class, shift @args);
+    my ($class, $controller, @args) = @_;
+    my $obj   = build($class => $controller);
     my $hooks = $obj->complex->{hooks};
-    $obj->node->invoke(sub {
+    $class->node->invoke(sub {
         my ($self, $context) = @_;
         if (my $codes = $hooks->{$self->{path_cached} || $self->get_path}) {
             for my $code (@{$codes}) {
